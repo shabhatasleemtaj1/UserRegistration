fda0f5aaae308a5da697fc04ae5e2e37837c7bfc
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-06 14:55:08 +0530

    UserRegistration added

--- UserRegistration.scala
+++ UserRegistration.scala
@@ -3,25 +3,6 @@ import scala.io.StdIn._
 object RegularExpression1 {
   def main(args: Array[String]): Unit = {
     println("Welcome to User Registration")
-    var FirstName=readLine("Enter First Name:")
-    val FirstNamepattern ="^[A-Z]{1,}[a-z]{3,}$".r
-    if(FirstNamepattern.matches(FirstName))
-    {
-      println("FirstName is valid:")
-    }
-    else
-    {
-      println("FirstName is not valid")
-    }
-  }
-
-}
-//usecase2
-import scala.util.matching.Regex
-import scala.io.StdIn._
-object RegularExpression1 {
-  def main(args: Array[String]): Unit = {
-    println("Welcome to User Registration")
     val FirstNamepattern = "^[A-Z]{1,}[a-z]{3,}$".r
     val LastNamepattern = "^[A-Z]{1,}[a-z]{3,}$".r
     var FirstName = readLine("Enter First Name:")
