2d8dbc3d3b5339a9319a79a863e42ebdfca9f3b6
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-06 19:39:54 +0530

    Usecase 5 added

--- UserRegistration.scala
+++ UserRegistration.scala
@@ -1,3 +1,4 @@
+//usecase 5
 import scala.util.matching.Regex
 import scala.io.StdIn._
 object RegularExpression1 {
@@ -6,6 +7,7 @@ object RegularExpression1 {
     val FirstLastNamePattern = "^[A-Z]{1,}[a-z]{3,}$".r
     val EmailPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,3})$".r
     val MobileNumberPattern=("^([0-9]{2,3})\\s{1}[0-9]{10}").r
+    val pattern="^(?=.*[a-z])(?=.*[A-Z]).{8,}$".r
     var FirstName = readLine("Enter First Name:")
     if (FirstLastNamePattern.matches(FirstName)) {
       println("First Name is valid")
@@ -36,5 +38,15 @@ object RegularExpression1 {
     {
       println("MobileNumber is not valid")
     }
+    val Password = readLine("Enter your Password:")
+    if(PasswordPattern.matches(Password))
+    {
+      println("Password is valid:")
+      else
+      {
+        println("Password is not valid")
+      }
+    }
+    println("Registration is successful ,Thank you")
   }
 }
