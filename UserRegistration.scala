81e3840d37d5249035d8f9fbd5e81a359bde5d72
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-06 17:52:06 +0530

    Usecase 3 added

--- UserRegistration.scala
+++ UserRegistration.scala
@@ -1,25 +1,31 @@
+//use case3 
 import scala.util.matching.Regex
 import scala.io.StdIn._
 object RegularExpression1 {
   def main(args: Array[String]): Unit = {
     println("Welcome to User Registration")
-    val FirstNamepattern = "^[A-Z]{1,}[a-z]{3,}$".r
-    val LastNamepattern = "^[A-Z]{1,}[a-z]{3,}$".r
+    val FirstLastNamePattern = "^[A-Z]{1,}[a-z]{3,}$".r
+    val EmailPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,3})$".r
     var FirstName = readLine("Enter First Name:")
-    if (FirstNamepattern.matches(FirstName)) {
-      println("FirstName is valid:")
+    if (FirstLastNamePattern.matches(FirstName)) {
+      println("First Name is valid")
     }
     else {
-      println("FirstName is not valid)
+      println("First name is invalid")
     }
     var LastName = readLine("Enter Last Name:")
-    if (LastNamepattern.matches(LastName)) {
-      println("LastName is valid:")
+    if (FirstLastNamePattern.matches(LastName)) {
+      println("Last Name is valid")
     }
     else {
-      println("LastName is not valid")
+      println("Last name is invalid")
+    }
+    var Email = readLine("Enter an Email:")
+    if (EmailPattern.matches(Email)) {
+      println("Email is valid")
+    }
+    else {
+      println("Email is invalid")
     }
   }
 }
-
-
