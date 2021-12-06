906346249a7f89a9a5edbbc019ba4b202c429766
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-06 13:25:35 +0530

    UserRegistration added

---
+++ UserRegistration.scala
@@ -0,0 +1,17 @@
+import scala.util.matching.Regex
+import scala.io.StdIn._
+object RegularExpression1 {
+  def main(args: Array[String]): Unit = {
+    println("Welcome to User Registration")
+    var FirstName=readLine("Enter First Name:")
+    val FirstNamepattern ="^[A-Z]{1,}[a-z]{3,}$".r
+    if(FirstNamepattern.matches(FirstName))
+    {
+      println("FirstName is valid:")
+    }
+    else
+    {
+      println("FirstName is not valid")
+    }
+  }
+}
