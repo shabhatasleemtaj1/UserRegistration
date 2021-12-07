d047416daa76230bf18ca7dcd1696b4d0e21551d
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-07 16:17:36 +0530

    UserRegistration added

--- UserRegistration.scala
+++ UserRegistration.scala
@@ -1,46 +1,32 @@
- import scala.util.matching.Regex
+//Use case 8
+import scala.util.matching.Regex
 import scala.io.StdIn._
 object RegularExpression1 {
   def main(args: Array[String]): Unit = {
     println("Welcome to User Registration")
-    val FirstLastNamePattern = "^[A-Z]{1,}[a-z]{3,}$".r
-    val EmailPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,3})$".r
-    val MobileNumberPattern = ("^([0-9]{2,3})\\s{1}[0-9]{10}").r
-    val PasswordPattern ="^(?=.*?[A-Z][a-z])(?=.*?[0-9]).{8,}$".r
-    var FirstName = readLine("Enter First Name:")
-    if (FirstLastNamePattern.matches(FirstName)) {
-      println("First Name is valid")
+    val firstLastNamePattern= "^[A-Z]{1,}[a-z]{3,}$"
+    val emailPattern = "^[^\\.][a-z0-9\\.\\-+]+@[a-z0-9]{1,}\\.[a-z]{2,3}(\\.[a-z]{2,3})?$"
+    val mobileNumberPattern = ("^([0-9]{2,3})\\s{1}[0-9]{10}")
+    val passwordPattern = "^(?=.*?[A-Z])(?=.*?[a-z(?=.*?[0-9])])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+    var firstName = readLine("Enter First Name:")
+    var firstNameCheck = validatePattern(firstLastNamePattern, firstName)
+    var lastName = readLine("Enter Last Name:")
+    var lastNameCheck = validatePattern(firstLastNamePattern, lastName)
+    var emailid = readLine("Enter an Email:")
+    var emailIdCheck = validatePattern(emailPattern, emailid)
+    var mobileNumber = readLine("Enter Mobile Number:")
+    var mobileNumberCheck = validatePattern(mobileNumberPattern, mobileNumber)
+    var password = readLine("Enter your Password:")
+    var passwordCheck = validatePattern(passwordPattern, password)
+  }
+  def validatePattern(pattern: String, temp1: String): Boolean = {
+    var tempData: String = "";
+    tempData = temp1
+    while (tempData.matches(pattern) == false) {
+      tempData = readLine("Please enter correct data :")
     }
-    else {
-      println("First name is invalid")
-    }
-    var LastName = readLine("Enter Last Name:")
-    if (FirstLastNamePattern.matches(LastName)) {
-      println("Last Name is valid")
-    }
-    else {
-      println("Last name is invalid")
-    }
-    var Email = readLine("Enter an Email:")
-    if (EmailPattern.matches(Email)) {
-      println("Email is valid")
-    }
-    else {
-      println("Email is invalid")
-    }
-    var MobileNumber = readLine("Enter Mobile Number:")
-    if (MobileNumberPattern.matches(MobileNumber)) {
-      println("MobileNumber is valid:")
-    }
-    else {
-      println("MobileNumber is not valid")
-    }
-    val Password = readLine("Enter your Password:")
-    if (PasswordPattern.matches(Password)) {
-      println("Password is valid:")
-    }
-    else {
-      println("Password is not valid")
-    }
+    println("Inserted details accepted.")
+    return true
   }
 }
+
